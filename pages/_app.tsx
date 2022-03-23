50acf74dd83a2d25b449eb63c98b3c1fa7cb3c7c
lws803 <lws803@gmail.com>
2022-03-23 23:13:30 +0000

    reformatted page a little more

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,23 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from "next/app";
+import { Global, MantineProvider } from "@mantine/core";
+
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <MantineProvider theme={{ colorScheme: "dark" }}>
+      <Global
+        styles={(theme) => ({
+          body: {
+            backgroundColor:
+              theme.colorScheme === "dark" ? theme.colors.dark[7] : theme.white,
+          },
+        })}
+      />
+
+      <Component {...pageProps} />
+    </MantineProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
