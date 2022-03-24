10606bf67024d321dda2082bf486ab2a63b03036
lws803 <lws803@gmail.com>
2022-03-24 22:30:08 +0000

    modified toolbar

--- components/RichTextEditor.tsx
+++ components/RichTextEditor.tsx
@@ -3,7 +3,7 @@ import Editor, { Quill } from "@mantine/rte";
 import MarkdownShortcuts from "quill-markdown-shortcuts";
 
 export default function RichTextEditor() {
-  const [value, setValue] = useState("Hello world");
+  const [value, setValue] = useState("Start typing something...");
   const modules = useMemo(
     () => ({
       history: { delay: 2500, userOnly: true },
@@ -20,8 +20,15 @@ export default function RichTextEditor() {
         height: "100%",
       })}
       value={value}
+      placeholder="Start typing something..."
       onChange={setValue}
       modules={modules}
+      controls={[
+        ["bold", "italic", "underline", "link", "image"],
+        ["unorderedList", "h1", "h2", "h3"],
+        ["sup", "sub"],
+        ["code", "codeBlock"],
+      ]}
     />
   );
 }
